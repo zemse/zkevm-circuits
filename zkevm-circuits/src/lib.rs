475a15d81246b54e85472991fdeaffd290cef391
Soham Zemse <22412996+zemse@users.noreply.github.com>
2024-01-23 17:05:55 +0530

    upgrade to halo2 v0.3.0

--- zkevm-circuits/src/lib.rs
+++ zkevm-circuits/src/lib.rs
@@ -34,6 +34,7 @@ pub mod keccak_circuit;
 pub mod mpt_circuit;
 pub mod pi_circuit;
 #[cfg(not(target_arch = "wasm32"))]
+#[cfg(feature = "snark-verifier")]
 pub mod root_circuit;
 pub mod state_circuit;
 pub mod super_circuit;
